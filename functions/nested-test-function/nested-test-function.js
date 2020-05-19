454728bea5cfb16aacfe73593d91ec1cb4515c83
Ole Strøm <olestr@pm.me>
2020-05-19 15:33:20 +0200

    feat: add proof of concept

---
+++ functions/nested-test-function/nested-test-function.js
@@ -0,0 +1,20 @@
+const {capitalize} = require('../../utilities/index.js')
+
+
+exports.handler = async function(event, context) {
+    try {
+        return {
+            statusCode: 200,
+            body: capitalize('NETLIFY IS GREAT')
+        }
+    }
+
+    catch (error) {
+        console.warn(error)
+
+        return {
+            statusCode: 500,
+            body: error.toString()
+        }
+    }
+}
